a4ee54b11d4a5dea88249431a38c088e2c64a5dd
Ninshad <ninshad7@gmail.com>
2023-04-11 18:33:53 +0530

    11/4/23

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import FC from 'react'
+
+interface PageProps {}
+
+const page:FC<PageProps> = () => {
+  return <div>Heyyy</div>
+}
+
+export default page
---
+++ src/pages/api/auth/[...nextauth].tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface Props {}
+
+const [...nextauth] = () => {
+  return <div></div>
+}
+
+export default [...nextauth]
